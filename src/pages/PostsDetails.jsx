36c62bd4b948b12405bb15ee5b1921bfa586b435
Valiyulloh0221 <lvov11190@gmail.com>
2024-03-15 11:02:52 +0500

    jnefij

---
+++ src/pages/PostsDetails.jsx
@@ -0,0 +1,7 @@
+import Posts from "../components/Hero/Posts";
+import PostsCard from "../components/Hero/PostsCard";
+const PostsDetails = () => {
+  return <div>PostsDetails</div>;
+};
+
+export default PostsDetails;
